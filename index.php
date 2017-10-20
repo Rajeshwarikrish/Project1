9ab3f6a60d190f74c4c1ff93ad9eb12e4814e914
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-10-20 18:18:46 -0400

    Clearing unwanted code

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 class Manage {
-public static function autoload($class) {
-include $class . '.php';
+public static function autoload($class){
+	include $class . '.php';
 	}
 }
 spl_autoload_register(array('Manage' , 'autoload'));
@@ -13,94 +13,85 @@ $obj = new main();
 class main {
 	public function __construct()
 	{
-	$pageRequest = 'homepage';
-	if(isset($_REQUEST['page'])) {
-	  $pageRequest = $_REQUEST['page'];
-	 }
+	   $pageRequest = 'homepage';
+	   if(isset($_REQUEST['page'])) {
+	   $pageRequest = $_REQUEST['page'];
+	   }
 	   $page = new $pageRequest;
-
-	 if($_SERVER['REQUEST_METHOD'] =='GET') {
-	 	$page->get();
-	} else {
-		$page->post();
-	}
-    }
+           if($_SERVER['REQUEST_METHOD'] =='GET') {
+	   $page->get();
+	   } else {
+       	   $page->post();
+	   }
+      }
 }
 
 abstract class page {
-     protected $html;
-
-     public function __construct()
+     	protected $html;
+     
+     	public function __construct()
      {
-     	$this->html .= '<html>';
-	$this->html .= '<link rel="stylesheet" href="styles.css">';
-	$this->html .= '<body>';
+     	 $this->html .= '<html>';
+	 $this->html .= '<link rel="stylesheet" href="styles.css">';
+	 $this->html .= '<body>';
      }
-     public function __destruct()
+     
+     	public function __destruct()
      {
-     	$this->html .= '</body></html>';
-	stringFunctions::printThis($this->html);
+     	 $this->html .= '</body></html>';
+	 stringFunctions::printThis($this->html);
+     }
+     
+     	public function get() {
+     	 echo 'default get message';
      }
 
-     public function get() {
-     	echo 'default get message';
-      }
-
-      public function post() {
-      	print_r($_POST);
-      }
+	public function post() {
+      	 print_r($_POST);
+     }
 }
 
-class homepage extends page {
-	
-	public function get() {
-
-	$form = '<form action="index.php" method="post" enctype="multipart/form-data">';
-	$form .= '<h3><center>Select the CSV File to be uploaded:<br></center></h3>';
-	$form .= '<center>';
-	$form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
-	$form .= '<br>';
-	$form .= '<input type="submit" value="submit">';
-	$form .= '</center>';
-	$form .= '</form> ';
-	$this->html .= '<center><h2>Upload Page</h2></center>';
-	$this->html .= $form;						
-      }
-      public function post() {
-    /*      $tardir = "uploads/";
-	  print_r($_FILES);
-	  $tarFile = $tardir . $_FILES["fileToUpload"]["name"];
-	  $fileType = pathinfo($tarFile,PATHINFO_EXTENSION);
-	  if(isset($_POST["Submit"]))  {
-	      $soucefile=$_FILES["fileToUpload"]["tmp_name"];
-	       move_uploaded_file($sourcefile,$tarFile);
-	       echo 'File Uploaded Successfully';
-	       */
-
-	    //if(isset($_POST["submit"])) {
-	    
-	    $sourcefile = $_FILES["fileToUpload"]["name"];
-	    $tempname = $_FILES["fileToUpload"]["tmp_name"];
-	   // print_r($_FILES);
-	    $filename = uploadfile::csvfileupload($sourcefile,$tempname);
-	    //echo $filename;
-	    header('Location:?page=table&filename='.$filename);
-	   // }
-	 }
-    }
-     class table extends page
-     {
-	 public function get() 
+class homepage extends page 
+{
+	public function get()
+	{
+		$form = '<form action="index.php" method="post" enctype="multipart/form-data">';
+		$form .= '<h3><center>Select the CSV File to be uploaded:<br></center></h3>';
+		$form .= '<center>';
+		$form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+		$form .= '<br>';
+		$form .= '<input type="submit" value="submit">';
+		$form .= '</center>';
+		$form .= '</form> ';
+		$this->html .= '<center><h2>Upload Page</h2></center>';
+		$this->html .= $form;						
+        }
+        
+	public function post() 
+	{
+		$sourcefile = $_FILES["fileToUpload"]["name"];
+		$tempname = $_FILES["fileToUpload"]["tmp_name"];
+		$filename = uploadfile::csvfileupload($sourcefile,$tempname);
+		header('Location:?page=table&filename='.$filename);
+       	}
+}
+  
+class table extends page
+{
+	 public function get()
 	 {
 	    $sourcefile=$_GET['filename'];
 	    echo trim($sourcefile,"uploads/"). "is successfully uploaded<br><br> The Table is as shown below<br>";
 	    $heading = 1;
 	    $handle = fopen($sourcefile,"r");
 	    $table = '<table border="1">';
-	    while(($data = fgetcsv($handle))!=FALSE) {
-	       if ($heading == 1) {
+	    while(($data = fgetcsv($handle))!=FALSE)
+	    {
+	       if ($heading == 1)
+	       {
 	         $table .='<thead><tr>';
-		   foreach ($data as $value) {
+		   foreach ($data as $value) 
+		   {
 		     if (!isset($value))
 		       $value = "&nbsp";
 		     else
@@ -110,40 +101,40 @@ class homepage extends page {
 		}
                 else {
 		      $table .='<tr>';
-		      foreach ($data as $value)  {
+		      foreach ($data as $value)  
+		      {
 		        if(!isset($value))
 			  $value = "&nbsp";
 			else
 			  $table .= "<td>". $value . "</td>";
 		      $table .='</tr>';
   	       	      }
-	       }
-	}
-	$heading++;
-	$table .= '</tbody></table>';
-	$this->html .= $table;
-	fclose($handle);
-	stringFunctions::printThis($this->html);
+	        }
+	  }
+	 $heading++;
+	 $table .= '</tbody></table>';
+	 $this->html .= $table;
+	 fclose($handle);
+	 stringFunctions::printThis($this->html);
      	}
-    }
+}
 class uploadfile
 {
-    public static function csvfileupload($sourcefile,$tmpname) {
-    $tardir = "uploads/";
-   // print_r($_FILES);
-   // $tarFile = $tardir . $_FILES["fileToUpload"]["name"];
-    $tarfile = $tardir. $sourcefile;
-    $fileType = pathinfo($tarfile,PATHINFO_EXTENSION);
-    //if(isset($_POST["submit"]))  {
-     // $tempfile = $_FILES["selectFile"]["tmp_name"];
-     $tempfile = $tmpname;
-     move_uploaded_file($tempfile,$tarfile);
-      	return $tarfile;
-        //echo 'File Uploaded Successfully';
-    	//}
-    }
+    	public static function csvfileupload($sourcefile,$tmpname) 
+	{
+    		$tardir = "uploads/";
+   		// print_r($_FILES);
+    		$tarfile = $tardir. $sourcefile;
+    		$fileType = pathinfo($tarfile,PATHINFO_EXTENSION);
+     		$tempfile = $tmpname;
+     		move_uploaded_file($tempfile,$tarfile);
+      		return $tarfile;
+        	//echo 'File Uploaded Successfully';
+    	}
 }
-class stringFunctions {
+
+class stringFunctions 
+{
   public static function printThis($text)  {
   print($text);
   }
